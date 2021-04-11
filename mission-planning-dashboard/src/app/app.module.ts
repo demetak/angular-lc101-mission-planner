4eab1f6d952de56041a5d6974bf2b5da883a98ed
unknown <demetakcaoglu@gmail.com>
2021-04-11 13:54:14 -0500

    Week 9B Studio completed with Bonus tasks

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Header } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
